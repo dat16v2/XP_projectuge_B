8ecc4f43d6acb317640c349fc0b6ea8351efc70c
Emil Hummel <sequoiia@hummel.yt>
2017-03-17 12:50:35 +0100

    Sample ratings

--- database/kino_database_sample.sql
+++ database/kino_database_sample.sql
@@ -9,6 +9,13 @@ INSERT INTO `genre` (name) VALUES ('Horror');
 INSERT INTO `genre` (name) VALUES ('War film');
 INSERT INTO `genre` (name) VALUES ('Science Fiction');
 
+# CREATE SAMPLE RATING
+INSERT INTO `genre` (name) VALUES ('3');
+INSERT INTO `genre` (name) VALUES ('8');
+INSERT INTO `genre` (name) VALUES ('12');
+INSERT INTO `genre` (name) VALUES ('16');
+INSERT INTO `genre` (name) VALUES ('18+');
+
 # CREATE SAMPLE ACTORS.
 INSERT INTO `actor` (first_name, last_name) VALUES ('Leonardo', 'DiCaprio');
 INSERT INTO `actor` (first_name, last_name) VALUES ('Johnny', 'Depp');
@@ -17,9 +24,10 @@ INSERT INTO `actor` (first_name, last_name) VALUES ('Anne', 'Hathaway');
 INSERT INTO `actor` (first_name, last_name) VALUES ('Scarlett', 'Johansson');
 INSERT INTO `actor` (first_name, last_name) VALUES ('Emma', 'Stone');
 
-# CREATE SHOW
+# CREATE SAMPLE SHOW
 INSERT INTO `show` (title, runtime, poster_path) VALUES ('Saving Private Ryan', 180, 'imgs/spr180px.jpg');
 SET @show_id = LAST_INSERT_ID();
 INSERT INTO `show_genre` (id_genre, id_show) VALUES (4, @show_id); # ADD GENRE
 INSERT INTO `show_actor` (id_actor, id_show) VALUES (3, @show_id); # ADD ACTOR
 INSERT INTO `show_actor` (id_actor, id_show) VALUES (4, @show_id); # ADD ACTOR
+INSERT INTO `show_rating` (id_rating, id_show) VALUES (5, @show_id); # ADD RATING
