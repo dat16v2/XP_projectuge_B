5309c6c19a66317775ed8371b74ba33234b86b9a
EkErilaz <x@x.dk>
2017-03-17 12:13:43 +0100

    showlisten addet

---
+++ src/model/ShowList.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by x on 17/03/2017.
+ */
+public class ShowList
+{
+    ArrayList<Show> listOfShows = new ArrayList<Show>();
+
+    public void add(String title, ArrayList<String> actorsList, int runTime, String ageLimit, String genre, String image) {
+
+    }
+
+    public void delete()
+    {
+
+    }
+
+    public void edit()
+    {
+
+    }
+
+    public void getListOfShow()
+    {
+
+    }
+
+}
