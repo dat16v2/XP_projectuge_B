c0555e8a2d61c4fb8b73cf305457b6d63d99428e
Emil Hummel <sequoiia@hummel.yt>
2017-03-17 11:27:00 +0100

    Begun work on basic DB structure

--- database/kino_database.sql
+++ database/kino_database.sql
@@ -1,1 +1,15 @@
-CREATE DATABASE IF NOT EXISTS kino;
+CREATE USER 'xp'@'localhost' IDENTIFIED BY 'password';
+/* CREATE USER 'xp'@'*' IDENTIFIED BY 'password'; */
+
+CREATE DATABASE IF NOT EXISTS xp_projectuge_b;
+
+GRANT ALL PRIVILEGES ON 'xp_projectuge_b' . * TO 'xp'@'localhost';
+GRANT ALL PRIVILEGES ON 'xp_projectuge_b' . * TO 'xp'@'*';
+
+FLUSH PRIVILEGES;
+
+use xp_projectuge_b;
+
+CREATE TABLE 'show'(
+	id INT NOT NULL AUTO_INCREMENT
+);
