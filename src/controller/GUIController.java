69f1d4c9f4e4c90afb1b1e0ea8ef8545c38c25ae
Emil Hummel <sequoiia@hummel.yt>
2017-03-21 14:37:11 +0100

    Reformatted GUIController

--- src/controller/GUIController.java
+++ src/controller/GUIController.java
@@ -4,27 +4,25 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+
 import java.io.IOException;
 
 public class GUIController {
 
-        public static void addShowWindow(ActionEvent actionEvent) throws IOException {
-            Parent newScene = FXMLLoader.load(Main.cl.getResource("view/addShowScene.fxml"));
-            Main.ps.setScene(new Scene(newScene, 650, 400));
-        }
+    public static void addShowWindow(ActionEvent actionEvent) throws IOException {
+        Parent newScene = FXMLLoader.load(Main.cl.getResource("view/addShowScene.fxml"));
+        Main.ps.setScene(new Scene(newScene, 650, 400));
+    }
 
-        public static void mainWindow(ActionEvent actionEvent) throws IOException {
-            //Parent newScene = FXMLLoader.load(getClass().getClassLoader().getResource("view/mainScene.fxml"));
-            Parent newScene = FXMLLoader.load(Main.cl.getResource("view/mainScene.fxml"));
-            Main.ps.setScene(new Scene(newScene, 1000, 600));
-        }
+    public static void mainWindow(ActionEvent actionEvent) throws IOException {
+        Main.ps.setScene(MainController.get().getScene());
+    }
 
-    public void showAddShowAlert(ActionEvent actionEvent) throws IOException
-    {
+    public void showAddShowAlert(ActionEvent actionEvent) throws IOException {
         view.Alertboxes.showAddShowAlertShow(actionEvent);
     }
 
-    public void addShowWindowFX (ActionEvent actionEvent) {
+    public void addShowWindowFX(ActionEvent actionEvent) {
         try {
             addShowWindow(actionEvent);
         } catch (IOException e) {
@@ -32,7 +30,7 @@ public class GUIController {
         }
     }
 
-    public void mainWindowFX (ActionEvent actionEvent) {
+    public void mainWindowFX(ActionEvent actionEvent) {
         try {
             mainWindow(actionEvent);
         } catch (IOException e) {
