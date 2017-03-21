d99f3f1799c43bc61cfe3320d780a21cd428361f
Nadia <nadia.jeppsson@gmail.com>
2017-03-21 11:40:23 +0100

    Changing pc, alertbox issues

--- src/controller/GUIController.java
+++ src/controller/GUIController.java
@@ -29,4 +29,28 @@ public class GUIController {
             Parent newScene = FXMLLoader.load(getClass().getClassLoader().getResource("view/mainScene.fxml"));
             Main.ps.setScene(new Scene(newScene, 1200, 800));
         }
+    public void showAddShowAlert(ActionEvent actionEvent) throws IOException
+    {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Er du sikker på at tilføje"  + " ?", ButtonType.YES, ButtonType.NO);
+        alert.showAndWait();
+
+        if (alert.getResult() == ButtonType.YES) {
+            alert.close();
+            Alert alert2 = new Alert(Alert.AlertType.CONFIRMATION, "Nu er filmen tilføjet", ButtonType.OK);
+            //alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+            alert2.show();
+
+            // Doesn't work as intended.
+            if (alert2.getResult() == ButtonType.OK) {
+                controller.GUIController.goBackToMain(actionEvent);
+
+                // Parent newScene = FXMLLoader.load(getClass().getClassLoader().getResource("view/mainScene.fxml"));
+                // Main.ps.setScene(new Scene(newScene, 1200, 800));
+
+            }
+        } else if (alert.getResult() == ButtonType.NO) {
+            alert.close();
+        }
+    }
 }
